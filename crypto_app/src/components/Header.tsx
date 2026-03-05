eb700f898b842f3c884b8e55bba303199af715a4
emarielouise972 <marielouiseesteban90@gmail.com>
2026-03-05 11:32:22 +0100

    modif header

--- crypto_app/src/components/Header.tsx
+++ crypto_app/src/components/Header.tsx
@@ -3,8 +3,17 @@ import "../styles/Header.css";
 
 export default function Header() {
   return (
+    <>
     <header className="header">
       <h1>Crypto app</h1>
+        <nav className="nav">
+            <ul>
+                <li><a href="#liste">Liste des cryptomonnaies</a></li>
+                <li><a href="#favoris">Favoris</a></li>
+                <li><a href="#connexion">Connexion</a></li>
+            </ul>
+        </nav>
     </header>
+    </>
   );
 }
