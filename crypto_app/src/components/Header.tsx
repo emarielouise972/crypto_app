f4b7b0e8f3cd8e2ed35fb6d5511c0f6d5bd885b4
emarielouise972 <marielouiseesteban90@gmail.com>
2026-03-05 10:46:55 +0100

    ajout du header

---
+++ crypto_app/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/Header.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Crypto app</h1>
+    </header>
+  );
+}
