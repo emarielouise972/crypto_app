29002c701175b5782ec06ddcdc8e43b35d824650
sitam rodier <matisrod91@gmail.com>
2026-03-24 19:10:16 +0100

    Faire en sorte que le graphe s'affiche bien sur la page

--- crypto_app/src/App.tsx
+++ crypto_app/src/App.tsx
@@ -1,22 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
 import CryptoList from './components/CryptoList'
-import GraphiqueCrypto from './components/Graphiques'
 
 function App() {
   return (
     <>
       <Header />
-      <div style={{ display: 'flex', gap: '20px', padding: '20px', marginTop: '100px' }}>
-        
+      <div className="main-content">
         <div>
           <CryptoList />
         </div>
-
-        <div style={{ flex: 1 }}>
-        </div>
-        
       </div>
     </> 
   )
